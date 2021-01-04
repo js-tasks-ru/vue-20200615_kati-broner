84c4b0b70967d28677f7bf6a2d262e55d9cce852
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-01-04 14:44:47 +0300

    Added solution for task: ContentTabs

--- 4-module/1-task/components/ContentTabs.vue
+++ 4-module/1-task/components/ContentTabs.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="content-tabs">
     <div class="content-tabs__nav">
-      <a href="#" class="content-tabs__tab">Page A</a>
-      <a href="#" class="content-tabs__tab">Page B</a>
+      <router-link
+        v-for="tab in tabs"
+        :key="tab.to.name || tab.to"
+        :to="tab.to"
+        class="content-tabs__tab"
+        active-class="content-tabs__tab_active"
+      >
+        {{ tab.text }}
+      </router-link>
     </div>
     <div class="content-tabs__content">
-      Content
+      <slot />
     </div>
   </div>
 </template>
@@ -13,6 +20,13 @@
 <script>
 export default {
   name: 'ContentTabs',
+
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
